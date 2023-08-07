5c23d4648e845ab0ce1f189c3df2bd7de6f25737
HE7086 <me@heyi7086.com>
2023-08-07 07:10:55 +0200

    add devshell

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
     nur.url = "github:nix-community/NUR";
     sops-nix.url = "github:Mic92/sops-nix";
     disko.url = "github:nix-community/disko";
+    flake-utils.url = "github:numtide/flake-utils";
  
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -11,20 +12,47 @@
     };
   };
 
-  outputs = { self, nixpkgs, nur, sops-nix, disko, home-manager, ... }@inputs:
-  let commonModules = [
-    sops-nix.nixosModules.sops
-    disko.nixosModules.disko
-    ./configuration.nix
-  ]; in {
-    nixosConfigurations = {
-      herd = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        system = "x86_64-linux";
-        modules = commonModules ++ [
-          ./hosts/herd.nix
+  outputs = {
+    self,
+    nixpkgs,
+    nur,
+    sops-nix,
+    disko,
+    flake-utils,
+    home-manager,
+    ...
+  }@inputs:
+  flake-utils.lib.eachDefaultSystem (system:
+    let 
+      pkgs = import nixpkgs { inherit system; };
+      commonModules = [
+        sops-nix.nixosModules.sops
+        disko.nixosModules.disko
+        ./configuration.nix
+      ];
+    in {
+      nixosConfigurations = {
+        herd = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          system = "x86_64-linux";
+          modules = commonModules ++ [
+            ./hosts/herd.nix
+          ];
+        };
+      };
+
+      devShells.default = pkgs.mkShellNoCC {
+        buildInputs = with pkgs; [
+          coreutils
+          git
+          just
+          rsync
+          curl
+          openssh
+          age
+          sops
         ];
       };
-    };
-  };
+    }
+  );
 }
