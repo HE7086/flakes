801b7a7a26cbcebd899fe420a16ac58d80dae4f0
HE7086 <me@heyi7086.com>
2023-08-08 08:39:53 +0200

    don't create symlink for host keys

--- modules/ssh-host-key.nix
+++ modules/ssh-host-key.nix
@@ -1,12 +1,6 @@
 { config, sops-nix, ... }: {
   sops.secrets = {
-    "${config.networking.hostName}/ssh_host_ed25519_key" = {
-      path = "/etc/ssh/ssh_host_ed25519_key";
-      mode = "0600";
-    };
-    "${config.networking.hostName}/ssh_host_ed25519_key.pub" = {
-      path = "/etc/ssh/ssh_host_ed25519_key.pub";
-      mode = "0644";
-    };
+    "${config.networking.hostName}/ssh_host_ed25519_key" = {};
+    "${config.networking.hostName}/ssh_host_ed25519_key.pub" = {};
   };
 }
