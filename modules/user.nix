8495be5df55d128ec3510fc68266750f53839b4a
HE7086 <me@heyi7086.com>
2023-08-08 08:57:47 +0200

    users

--- modules/user.nix
+++ modules/user.nix
@@ -3,14 +3,18 @@ let
   keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJgQ10239M1Ehw6nmY7mFxGyqfpCkfSHAjZzSZZZ7NLA" ];
 in
 {
+  # users.mutableUsers = false;
   users.defaultUserShell = pkgs.zsh;
   users.users = {
     he = {
       isNormalUser = true;
       home = "/home/he";
       extraGroups = [ "wheel" ];
+      uid = 1000;
       openssh.authorizedKeys.keys = keys;
     };
     root.openssh.authorizedKeys.keys = keys;
   };
+
+  security.sudo.wheelNeedsPassword = false;
 }
