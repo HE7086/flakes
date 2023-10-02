02d2b837e4d4a07bbe33f903567e41d4a8d3219a
HE7086 <me@heyi7086.com>
2023-10-02 17:44:55 +0200

    fix

--- hosts/fridge.nix
+++ hosts/fridge.nix
@@ -12,48 +12,27 @@
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
-
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.device = "nodev";
+
+  powerManagement.cpuFreqGovernor = "performance";
+  hardware.cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
+
+  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  boot.kernelParams = [ "nohibernate" ];
 
   networking.hostName = "fridge";
   networking.hostId = "83d9da0a";
 
   time.timeZone = "Europe/Berlin";
 
-  # fileSystems."/" =
-  #   { device = "zroot/root/nixos";
-  #     fsType = "zfs";
-  #   };
-
-  # fileSystems."/home" =
-  #   { device = "zroot/home";
-  #     fsType = "zfs";
-  #   };
-
-  # fileSystems."/boot" =
-  #   { device = "/dev/disk/by-uuid/1D4A-3A59";
-  #     fsType = "vfat";
-  #   };
-
-  # swapDevices =
-  #   [ { device = "/dev/disk/by-uuid/4e0920bd-6a33-4cdc-90e6-317efac7b2b0"; }
-  #   ];
 
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   # disko.devices = {
   #   disk.disk1 = {
