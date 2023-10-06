eefda839ed009aa02335d1d8783bcfbd29183ffb
HE7086 <me@heyi7086.com>
2023-10-06 14:41:47 +0200

    fmt

--- hosts/fridge.nix
+++ hosts/fridge.nix
@@ -33,41 +33,27 @@
     defaultGateway = "192.168.1.1";
     nameservers = [ "1.1.1.1" "8.8.8.8" ];
     interfaces = {
-      enp1s0 = {
-        useDHCP = false;
-      };
-      enp2s0 = {
-        useDHCP = false;
-      };
-      enp3s0 = {
-        useDHCP = false;
-      };
-      enp4s0 = {
-        useDHCP = false;
-      };
+      enp1s0.useDHCP = false;
+      enp2s0.useDHCP = false;
+      enp3s0.useDHCP = false;
+      enp4s0.useDHCP = false;
       br0 = {
         useDHCP = true;
-        ipv4 = {
-          addresses = [
-            {
-              address = "192.168.1.2";
-              prefixLength = 24;
-            }
-          ];
-        };
+        ipv4.addresses = [{
+          address = "192.168.1.2";
+          prefixLength = 24;
+        }];
       };
     };
 
-    bridges = {
-      br0 = {
-        interfaces = [
-          "enp1s0"
-          "enp2s0"
-          "enp3s0"
-          "enp4s0"
-        ];
-        rstp = true;
-      };
+    bridges.br0 = {
+      interfaces = [
+        "enp1s0"
+        "enp2s0"
+        "enp3s0"
+        "enp4s0"
+      ];
+      rstp = true;
     };
   };
 
