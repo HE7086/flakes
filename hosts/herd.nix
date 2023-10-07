7bec6150a8ac20b0c07815c5fbb8061a4329ecd6
HE7086 <me@heyi7086.com>
2023-10-07 22:22:24 +0200

    dns

--- hosts/herd.nix
+++ hosts/herd.nix
@@ -20,6 +20,10 @@
     routes = [
       { routeConfig.Gateway = "fe80::1"; }
     ];
+    dns = [
+      "1.1.1.1" "1.0.0.1"
+      "2606:4700:4700::1111" "2606:4700:4700::1001"
+    ];
   };
   networking = {
     hostName = "herd";
