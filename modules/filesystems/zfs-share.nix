94ff1fbbb1be86930fa1aba524211c7a021e3622
HE7086 <me@heyi7086.com>
2023-10-31 15:45:29 +0100

    redo zfs

--- modules/filesystems/zfs-share.nix
+++ modules/filesystems/zfs-share.nix
@@ -24,16 +24,26 @@ diskDevice: {
       acltype = "posixacl";
       dnodesize = "auto";
       normalization = "formD";
-      relatime = "on";
+      relatime = "off";
       "com.sun:auto-snapshot" = "false";
+      canmount="off";
     };
-    options.mountpoint = "legacy";
-    datasets = { };
+    mountpoint = null;
+    datasets = {
+      shared = {
+        type = "zfs_fs";
+        options.mountpoint = "legacy";
+        mountpoint = "/share";
+      };
+    };
   };
 
   boot.zfs.extraPools = [ "share" ];
   fileSystems."/share" = {
-    device = "share";
+    device = "share/shared";
     fsType = "zfs";
+    options = [ "noatime" "norelatime" ];
   };
+
+  services.zfs.autoScrub.enable = true;
 }
