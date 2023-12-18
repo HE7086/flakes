9e2cff67fd844321df666496a4a5292a105ca69f
HE7086 <me@heyi7086.com>
2023-12-18 10:08:56 +0100

    use disko

--- hosts/toaster.nix
+++ hosts/toaster.nix
@@ -6,35 +6,6 @@
   ];
   boot.initrd.availableKernelModules = [ "xhci_pci" "virtio_pci" "virtio_scsi" "usbhid" ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/12a35f9e-05e9-42fc-b6d6-45c36d5edf97";
-      fsType = "btrfs";
-      options = [ "subvol=@root" ];
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/74E3-662D";
-      fsType = "vfat";
-    };
-
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/12a35f9e-05e9-42fc-b6d6-45c36d5edf97";
-      fsType = "btrfs";
-      options = [ "subvol=@home" ];
-    };
-
-  fileSystems."/var/log" =
-    { device = "/dev/disk/by-uuid/12a35f9e-05e9-42fc-b6d6-45c36d5edf97";
-      fsType = "btrfs";
-      options = [ "subvol=@var_log" ];
-    };
-
-  fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/12a35f9e-05e9-42fc-b6d6-45c36d5edf97";
-      fsType = "btrfs";
-      options = [ "subvol=@nix" ];
-    };
-
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
