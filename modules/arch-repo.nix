6ee00f7ae8d460f1bfc63317ce1d11053622c36f
HE7086 <me@heyi7086.com>
2023-12-27 20:18:43 +0100

    add fancyindex

--- modules/arch-repo.nix
+++ modules/arch-repo.nix
@@ -1,9 +1,14 @@
-{
+{ pkgs, ... }: {
   services.nginx.virtualHosts = {
     "repo.heyi7086.com" = {
       forceSSL = true;
       enableACME = true;
       root = "/var/www/repo";
+      locations."/".extraConfig = ''
+          fancyindex on;              # Enable fancy indexes.
+          fancyindex_exact_size off;  # Output human-readable file sizes.
+      '';
     };
   };
+  services.nginx.additionalModules = [ pkgs.nginxModules.fancyindex ];
 }
