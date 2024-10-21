bb6cf8e0ad9ed4c65ea3f61ec40619be1a222144
HE7086 <me@heyi7086.com>
2024-10-21 19:18:32 +0200

    datadir

--- modules/suwayomi.nix
+++ modules/suwayomi.nix
@@ -2,6 +2,7 @@
   services.suwayomi-server = {
     enable = true;
     package = pkgs.unstable.suwayomi-server;
+    dataDir = "/share/Data/suwayomi-server";
     settings = {
       server = {
         port = 7375;
