8be62e34fc1391fb2962086c2b98510bc475fb3f
HE7086 <me@heyi7086.com>
2024-10-24 15:25:14 +0200

    update ddns

--- modules/ddns.nix
+++ modules/ddns.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }: {
+{ config, ... }: {
   sops.secrets."cloudflare/token/ddns" = {
     sopsFile = ../secrets/secrets.yaml;
   };
@@ -6,10 +6,7 @@
   services.cloudflare-dyndns = {
     enable = true;
     apiTokenFile = config.sops.secrets."cloudflare/token/ddns".path;
-    domains = [
-      "${config.networking.hostName}.${config.networking.domain}"
-      "*.${config.networking.hostName}.${config.networking.domain}"
-    ];
+    domains = [ "${config.networking.hostName}.${config.networking.domain}" ];
     proxied = false;
     ipv4 = true;
     ipv6 = true;
@@ -19,10 +16,5 @@
   systemd.timers.cloudflare-dyndns = {
     after = [ "network-online.target" ];
     wants = [ "network-online.target" ];
-    timerConfig = {
-      OnBootSec = "10s";
-      OnCalendar = lib.mkForce "*-*-* *:00/2:00";
-      RandomizedDelaySec = "10m";
-    };
   };
 }
