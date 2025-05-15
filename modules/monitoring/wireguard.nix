a3483ef532945d8d33b6137403f4200b041d5086
HE7086 <me@heyi7086.com>
2025-05-15 10:48:26 +0200

    update monitoring

--- modules/monitoring/wireguard.nix
+++ modules/monitoring/wireguard.nix
@@ -1,10 +1,12 @@
 { config, lib, ... }:
 {
-  services.prometheus.exporters.wireguard = lib.mkIf config.networking.wireguard.enable {
-    enable = true;
-    withRemoteIp = true;
-    singleSubnetPerField = true;
-  };
+  services.prometheus.exporters.wireguard =
+    lib.mkIf (config.networking.wireguard.enable || config.networking.wg-quick.interfaces != {})
+      {
+        enable = true;
+        withRemoteIp = true;
+        singleSubnetPerField = true;
+      };
   services.alloy.extraConfigs = with config.services.prometheus.exporters.wireguard; [
     ''
       prometheus.scrape "wireguard" {
